863dd3617abd77f916c8d9e2657f0a4871381ce1
ashaaneja11 <asha.aneja111@gmail.com>
2025-07-11 22:03:12 +0100

    Create array_list.java

---
+++ array_list.java
@@ -0,0 +1,12 @@
+import java.util.ArrayList;
+
+public class MyClass {
+  public static void main(String[] args) {
+      
+    ArrayList<String> studentsName = new ArrayList<>();
+    studentsName.add("rakesh");
+    // n + n/2 + 1
+
+     System.out.println(studentsName[]);
+  }
+}
